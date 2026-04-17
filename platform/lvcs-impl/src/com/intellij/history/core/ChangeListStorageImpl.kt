d8a475ec727a6edc9ab2be7f96a2eaa0727bd77e
Ivan Semenov <ivan.semenov@jetbrains.com>
2026-04-17 12:39:33 +0200

    refactor [lvcs]: convert to Kotlin
    GitOrigin-RevId: 8c14106fb9ff0a0c7939b008d7257eccc2364d8a

--- platform/lvcs-impl/src/com/intellij/history/core/ChangeListStorageImpl.kt
+++ platform/lvcs-impl/src/com/intellij/history/core/ChangeListStorageImpl.kt
@@ -1,267 +1,259 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.history.core;
+package com.intellij.history.core
 
-import com.intellij.history.core.changes.ChangeSet;
-import com.intellij.history.integration.LocalHistoryBundle;
-import com.intellij.history.utils.LocalHistoryLog;
-import com.intellij.notification.NotificationType;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.util.Disposer;
-import com.intellij.openapi.util.Pair;
-import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.openapi.vfs.newvfs.ManagingFS;
-import com.intellij.util.Consumer;
-import com.intellij.util.io.ClosedStorageException;
-import com.intellij.util.io.storage.AbstractStorage;
-import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
-import it.unimi.dsi.fastutil.ints.IntSet;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.history.core.changes.ChangeSet
+import com.intellij.history.integration.LocalHistoryBundle
+import com.intellij.history.utils.LocalHistoryLog
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.vfs.newvfs.ManagingFS
+import com.intellij.util.Consumer
+import com.intellij.util.io.ClosedStorageException
+import com.intellij.util.io.delete
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet
+import it.unimi.dsi.fastutil.ints.IntSet
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.annotations.NonNls
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Path
+import java.text.DateFormat
 
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.text.DateFormat;
-import java.text.MessageFormat;
-
-import static com.intellij.history.core.LocalHistoryNotificationIdsHolder.STORAGE_CORRUPTED;
-import static com.intellij.history.core.LocalHistoryNotificationIdsHolderKt.getLocalHistoryNotificationGroup;
+private const val VERSION = 7
+private const val STORAGE_FILE: @NonNls String = "changes"
 
 @ApiStatus.Internal
-public final class ChangeListStorageImpl implements ChangeListStorage {
-  private static final int VERSION = 7;
-  private static final @NonNls String STORAGE_FILE = "changes";
-
-  private final Path myStorageDir;
+class ChangeListStorageImpl(private val storageDir: Path) : ChangeListStorage {
   //TODO RC: use mmapped storage instead of old-school? Less freezes, and also more reliability
-  private final LocalHistoryStorage myStorage;
-  private long myLastId;
+  private val storage: LocalHistoryStorage
+  private var lastId: Long = 0
 
-  private boolean isCompletelyBroken;
-  private final boolean myUnitTestMode;
+  private var isCompletelyBroken = false
+  private val unitTestMode = ApplicationManager.getApplication().isUnitTestMode()
 
-  public ChangeListStorageImpl(@NotNull Path storageDir) throws IOException {
-    myStorageDir = storageDir;
-    myUnitTestMode = ApplicationManager.getApplication().isUnitTestMode();
-
-    myStorage = initStorage(myStorageDir);
+  init {
+    storage = initStorage(storageDir)
   }
 
-  private synchronized LocalHistoryStorage initStorage(@NotNull Path storageDir) throws IOException {
-    Path path = storageDir.resolve(STORAGE_FILE);
+  @Synchronized
+  @Throws(IOException::class)
+  private fun initStorage(storageDir: Path): LocalHistoryStorage {
+    val path = storageDir.resolve(STORAGE_FILE)
+    val fromScratch = unitTestMode && !Files.exists(path)
 
-    boolean fromScratch = myUnitTestMode && !Files.exists(path);
+    var storage = LocalHistoryStorage(path)
 
-    LocalHistoryStorage result = new LocalHistoryStorage(path);
+    val fsTimestamp = ManagingFS.getInstance().getCreationTimestamp()
 
-    long fsTimestamp = getVFSTimestamp();
-
-    int storedVersion = result.getVersion();
-    boolean versionMismatch = storedVersion != VERSION;
-    boolean timestampMismatch = result.getFSTimestamp() != fsTimestamp;
+    val storedVersion = storage.getVersion()
+    val versionMismatch = storedVersion != VERSION
+    val timestampMismatch = storage.getFSTimestamp() != fsTimestamp
     if (versionMismatch || timestampMismatch) {
       if (!fromScratch) {
         if (versionMismatch) {
-          LocalHistoryLog.LOG.info(MessageFormat.format(
-            "local history version mismatch (was: {0}, expected: {1}), rebuilding...", storedVersion, VERSION));
+          LocalHistoryLog.LOG.info("local history version mismatch (was: $storedVersion, expected: $VERSION), rebuilding...")
         }
-        if (timestampMismatch) LocalHistoryLog.LOG.info("FS has been rebuild, rebuilding local history...");
-        Disposer.dispose(result);
-        FileUtil.delete(storageDir);
-        result = new LocalHistoryStorage(path);
+        if (timestampMismatch) {
+          LocalHistoryLog.LOG.info("FS has been rebuild, rebuilding local history...")
+        }
+        Disposer.dispose(storage)
+        storageDir.delete()
+        storage = LocalHistoryStorage(path)
       }
-      result.setVersion(VERSION);
-      result.setFSTimestamp(fsTimestamp);
+      storage.setVersion(VERSION)
+      storage.setFSTimestamp(fsTimestamp)
     }
 
-    myLastId = result.getLastId();
-    return result;
+    lastId = storage.getLastId()
+    return storage
   }
 
-  private static long getVFSTimestamp() {
-    return ManagingFS.getInstance().getCreationTimestamp();
-  }
-
-  private void handleError(Throwable e, @Nullable @NonNls String message) {
-    if (e instanceof ClosedStorageException) {
-      return;
-    }
-    long storageTimestamp = -1;
-
-    long vfsTimestamp = getVFSTimestamp();
-    long timestamp = System.currentTimeMillis();
-
-    try {
-      storageTimestamp = myStorage.getFSTimestamp();
-    }
-    catch (Exception ex) {
-      LocalHistoryLog.LOG.warn("cannot read storage timestamp", ex);
+  private fun handleError(e: Throwable?, @NonNls message: @NonNls String?) {
+    if (e is ClosedStorageException) {
+      return
     }
 
-    String fullMsg = "Local history is broken" +
-                     "(version:" + VERSION +
-                     ", current timestamp: " + DateFormat.getDateTimeInstance().format(timestamp) +
-                     ", storage timestamp: " + DateFormat.getDateTimeInstance().format(storageTimestamp) +
-                     ", vfs timestamp: " + DateFormat.getDateTimeInstance().format(vfsTimestamp) +
-                     ", path: " + myStorageDir +
-                     ")\n" + message;
-    if (myUnitTestMode) {
-      LocalHistoryLog.LOG.warn(fullMsg, e);
+    val vfsTimestamp = ManagingFS.getInstance().getCreationTimestamp()
+    val timestamp = System.currentTimeMillis()
+
+    val storageTimestamp = try {
+      storage.getFSTimestamp()
+    }
+    catch (ex: Exception) {
+      LocalHistoryLog.LOG.warn("cannot read storage timestamp", ex)
+      -1L
+    }
+
+    val fullMsg = "Local history is broken " +
+                  "(version:" + VERSION +
+                  ", current timestamp: " + DateFormat.getDateTimeInstance().format(timestamp) +
+                  ", storage timestamp: " + DateFormat.getDateTimeInstance().format(storageTimestamp) +
+                  ", vfs timestamp: " + DateFormat.getDateTimeInstance().format(vfsTimestamp) +
+                  ", path: " + storageDir +
+                  ")\n" + message
+    if (unitTestMode) {
+      LocalHistoryLog.LOG.warn(fullMsg, e)
     }
     else {
-      LocalHistoryLog.LOG.error(fullMsg, e);
+      LocalHistoryLog.LOG.error(fullMsg, e)
     }
 
-    Disposer.dispose(myStorage);
+    Disposer.dispose(storage)
     try {
-      FileUtil.delete(myStorageDir);
-      initStorage(myStorageDir);
+      storageDir.delete()
+      initStorage(storageDir)
     }
-    catch (Throwable ex) {
-      LocalHistoryLog.LOG.error("cannot recreate storage", ex);
-      synchronized (this){
-        isCompletelyBroken = true;
+    catch (ex: Throwable) {
+      LocalHistoryLog.LOG.error("cannot recreate storage", ex)
+      synchronized(this) {
+        isCompletelyBroken = true
       }
     }
 
     getLocalHistoryNotificationGroup()
-      .createNotification(LocalHistoryBundle.message("notification.title.local.history.broken"),
-                          LocalHistoryBundle.message("notification.content.local.history.broken"),
-                          NotificationType.ERROR)
-      .setDisplayId(STORAGE_CORRUPTED)
-      .notify(null);
+      .createNotification(
+        LocalHistoryBundle.message("notification.title.local.history.broken"),
+        LocalHistoryBundle.message("notification.content.local.history.broken"),
+        NotificationType.ERROR
+      )
+      .setDisplayId(LocalHistoryNotificationIdsHolder.STORAGE_CORRUPTED)
+      .notify(null)
   }
 
-  @Override
-  public void close() {
-    Disposer.dispose(myStorage);
+  override fun close() {
+    Disposer.dispose(storage)
   }
 
-  @Override
-  public void force() {
+  override fun force() {
     try {
-      myStorage.force();
+      storage.force()
     }
-    catch (IOException e) {
-      handleError(e, null);
+    catch (e: IOException) {
+      handleError(e, null)
     }
   }
 
-  @Override
-  public synchronized long nextId() {
-    return ++myLastId;
+  @Synchronized
+  override fun nextId(): Long {
+    return ++lastId
   }
 
-  @Override
-  public synchronized @Nullable ChangeSetHolder readPrevious(int id, IntSet recursionGuard) {
-    if (isCompletelyBroken) return null;
+  @Synchronized
+  override fun readPrevious(id: Int, recursionGuard: IntSet): ChangeSetHolder? {
+    if (isCompletelyBroken) return null
 
-    int prevId = 0;
+    var prevId = 0
     try {
-      prevId = id == -1 ? myStorage.getLastRecord() : doReadPrevSafely(id, recursionGuard);
-      if (prevId == 0) return null;
+      prevId = if (id == -1) {
+        storage.getLastRecord()
+      }
+      else {
+        storage.getPrevRecordSafely(id, recursionGuard)
+      }
+      if (prevId == 0) {
+        return null
+      }
 
-      return doReadBlock(prevId);
+      return storage.readBlock(prevId)
     }
-    catch (Throwable e) {
-      String message = null;
+    catch (e: Throwable) {
+      var message: String? = null
       if (prevId != 0) {
         try {
-          Pair<Long, Integer> prevOS = myStorage.getOffsetAndSize(prevId);
-          long prevRecordTimestamp = myStorage.getTimestamp(prevId);
-          int lastRecord = myStorage.getLastRecord();
-          Pair<Long, Integer> lastOS = myStorage.getOffsetAndSize(lastRecord);
-          long lastRecordTimestamp = myStorage.getTimestamp(lastRecord);
+          val prevOS = storage.getOffsetAndSize(prevId)
+          val prevRecordTimestamp = storage.getTimestamp(prevId)
+          val lastRecord = storage.getLastRecord()
+          val lastOS = storage.getOffsetAndSize(lastRecord)
+          val lastRecordTimestamp = storage.getTimestamp(lastRecord)
 
-          message = "invalid record is: " + prevId + " offset: " + prevOS.first + " size: " + prevOS.second
-                    + " (created " + DateFormat.getDateTimeInstance().format(prevRecordTimestamp) + ") "
-                    + "last record is: " + lastRecord + " offset: " + lastOS.first + " size: " + lastOS.second
-                    + " (created " + DateFormat.getDateTimeInstance().format(lastRecordTimestamp) + ")";
+          message = "invalid record is: " + prevId + " offset: " + prevOS.first + " size: " + prevOS.second +
+                    " (created " + DateFormat.getDateTimeInstance().format(prevRecordTimestamp) + ") " +
+                    "last record is: " + lastRecord + " offset: " + lastOS.first + " size: " + lastOS.second +
+                    " (created " + DateFormat.getDateTimeInstance().format(lastRecordTimestamp) + ")"
         }
-        catch (Exception e1) {
-          message = "cannot retrieve more debug info: " + e1.getMessage();
+        catch (e1: Exception) {
+          message = "cannot retrieve more debug info: " + e1.message
         }
       }
 
-      handleError(e, message);
-      return null;
+      handleError(e, message)
+      return null
     }
   }
 
-  private @NotNull ChangeSetHolder doReadBlock(int id) throws IOException {
-    try (DataInputStream in = myStorage.readStream(id)) {
-      return new ChangeSetHolder(id, new ChangeSet(in));
-    }
-  }
-
-  @Override
-  public synchronized void writeNextSet(ChangeSet changeSet) {
-    if (isCompletelyBroken) return;
+  @Synchronized
+  override fun writeNextSet(changeSet: ChangeSet) {
+    if (isCompletelyBroken) return
 
     try {
-      try (AbstractStorage.StorageDataOutput out = myStorage.writeStream(myStorage.createNextRecord(), true)) {
-        changeSet.write(out);
+      storage.writeStream(storage.createNextRecord(), true).use { out ->
+        changeSet.write(out)
       }
-      myStorage.setLastId(myLastId);
+      storage.setLastId(lastId)
     }
-    catch (IOException e) {
-      handleError(e, null);
+    catch (e: IOException) {
+      handleError(e, null)
     }
   }
 
-  @Override
-  public synchronized void purge(long period, int intervalBetweenActivities, Consumer<? super ChangeSet> processor) {
-    if (isCompletelyBroken) return;
+  @Synchronized
+  override fun purge(period: Long, intervalBetweenActivities: Int, processor: Consumer<in ChangeSet?>) {
+    if (isCompletelyBroken) return
 
-    IntSet recursionGuard = new IntOpenHashSet(1000);
-
+    val recursionGuard = IntOpenHashSet(1000)
     try {
-      int firstObsoleteId = findFirstObsoleteBlock(period, intervalBetweenActivities, recursionGuard);
-      if (firstObsoleteId == 0) return;
+      val firstObsoleteId = findFirstObsoleteBlock(period, intervalBetweenActivities, recursionGuard)
+      if (firstObsoleteId == 0) return
 
-      int eachBlockId = firstObsoleteId;
-
+      var eachBlockId = firstObsoleteId
       while (eachBlockId != 0) {
-        processor.consume(doReadBlock(eachBlockId).changeSet());
-        eachBlockId = doReadPrevSafely(eachBlockId, recursionGuard);
+        processor.consume(storage.readBlock(eachBlockId).changeSet)
+        eachBlockId = storage.getPrevRecordSafely(eachBlockId, recursionGuard)
       }
-      myStorage.deleteRecordsUpTo(firstObsoleteId);
+      storage.deleteRecordsUpTo(firstObsoleteId)
     }
-    catch (IOException e) {
-      handleError(e, null);
+    catch (e: IOException) {
+      handleError(e, null)
     }
   }
 
-  private int findFirstObsoleteBlock(long period, int intervalBetweenActivities, IntSet recursionGuard) throws IOException {
-    long prevTimestamp = 0;
-    long length = 0;
+  @Throws(IOException::class)
+  private fun findFirstObsoleteBlock(period: Long, intervalBetweenActivities: Int, recursionGuard: IntSet): Int {
+    var prevTimestamp = 0L
+    var length = 0L
 
-    int last = myStorage.getLastRecord();
+    var last = storage.getLastRecord()
     while (last != 0) {
-      long t = myStorage.getTimestamp(last);
-      if (prevTimestamp == 0) prevTimestamp = t;
+      val t = storage.getTimestamp(last)
+      if (prevTimestamp == 0L) prevTimestamp = t
 
-      long delta = prevTimestamp - t;
-      prevTimestamp = t;
+      val delta = prevTimestamp - t
+      prevTimestamp = t
 
       // we sum only intervals between changes during one 'day' (intervalBetweenActivities) and add '1' between two 'days'
-      length += delta < intervalBetweenActivities ? delta : 1;
+      length += if (delta < intervalBetweenActivities) delta else 1
 
-      if (length >= period) return last;
+      if (length >= period) return last
 
-      last = doReadPrevSafely(last, recursionGuard);
+      last = storage.getPrevRecordSafely(last, recursionGuard)
     }
 
-    return 0;
+    return 0
   }
+}
 
-  private int doReadPrevSafely(int id, IntSet recursionGuard) throws IOException {
-    recursionGuard.add(id);
-    int prev = myStorage.getPrevRecord(id);
-    if (!recursionGuard.add(prev)) throw new IOException("Recursive records found");
-    return prev;
+@Throws(IOException::class)
+private fun LocalHistoryStorage.getPrevRecordSafely(id: Int, recursionGuard: IntSet): Int {
+  recursionGuard.add(id)
+  val prev = getPrevRecord(id)
+  if (!recursionGuard.add(prev)) throw IOException("Recursive records found")
+  return prev
+}
+
+@Throws(IOException::class)
+private fun LocalHistoryStorage.readBlock(id: Int): ChangeSetHolder {
+  readStream(id).use { `in` ->
+    return ChangeSetHolder(id, ChangeSet(`in`))
   }
 }
