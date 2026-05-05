590f566669c85ed81ef06c6d33f5c8c84daf757b
Alexander Zolotov <zolotov@jetbrains.com>
2026-05-05 10:56:05 +0200

    cleanup [air]: move map extensions to shims module
    GitOrigin-RevId: 2f8828d2ac3542df1a6a8014b2c2ac21d80fd246

--- fleet/multiplatform.shims/srcCommonMain/fleet/multiplatform/shims/MutableMapExtensions.kt
+++ fleet/multiplatform.shims/srcCommonMain/fleet/multiplatform/shims/MutableMapExtensions.kt
@@ -1,5 +1,5 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package fleet.util
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package fleet.multiplatform.shims
 
 inline fun <K, V> MutableMap<K, V>.computeIfAbsentShim(key: K, f: (K) -> V): V {
   return get(key) ?: f(key).also { set(key, it) }
